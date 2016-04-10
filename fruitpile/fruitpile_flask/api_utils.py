90cdb7029bfc3a181863cabfc837cf47ad577f52
software-fool <dominic@thebinkles.net>
2016-04-10 21:25:39 +0100

    Add base class to REST resources for consitency
    Add a base class in between the REST endpoint classes and the
    flask_restful Resource class.  This allows the authentication
    process to be conducted for all end points.

---
+++ fruitpile/fruitpile_flask/api_utils.py
@@ -0,0 +1,23 @@
+# -*- mode: python -*-
+# Copyright (c) 2016 Dominic Binks (software-fool on github)
+# This file is part of Fruitpile.
+#
+# Fruitpile is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Fruitpile is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Fruitpile.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import print_function
+from flask_restful import Resource
+
+# Base class for all Fruitpile REST endpoints to allow things like
+# authentication to be done reliably across the API.
+class FruitpileResource(Resource):
+  pass
--- fruitpile/fruitpile_flask/v1/files.py
+++ fruitpile/fruitpile_flask/v1/files.py
@@ -15,11 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with Fruitpile.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
-from flask_restful import Resource, reqparse
+from flask_restful import reqparse
+from ..api_utils import FruitpileResource
 from ...fp_ops import Fruitpile
 import os
 
-class FruitpileFiles(Resource):
+class FruitpileFiles(FruitpileResource):
   def __init__(self, **kwargs):
     self.fp = Fruitpile(kwargs["fppath"])
     self.fp.open()
