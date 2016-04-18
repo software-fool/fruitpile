0317f02c43bb095ab6e2f1d628380c882d97da79
software-fool <dominic@thebinkles.net>
2016-04-18 22:30:11 +0100

    Add ability to create a fileset via REST API
    Enable the addition of a new fileset through the REST API.  This
    includes returning a URL to the user which contains the URL of the
    newly created fileset.

--- fruitpile/fruitpile_flask/v1/fileset.py
+++ fruitpile/fruitpile_flask/v1/fileset.py
@@ -15,14 +15,17 @@
 # You should have received a copy of the GNU General Public License
 # along with Fruitpile.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
-from .files import FruitpileFiles
-from .filesets import FruitpileFilesets
-from .fileset import FruitpileFileset
-from flask_restful import Api
+from flask_restful import reqparse, fields, marshal_with
+from ..api_utils import FruitpileResource
+from ...fp_ops import Fruitpile
+import os
 
-def init_v1_api(app, fppath):
-  api = Api(app, prefix="/v1")
-  api.add_resource(FruitpileFiles, '/files', resource_class_kwargs={"fppath":fppath}, endpoint='files')
-  api.add_resource(FruitpileFilesets, '/filesets', resource_class_kwargs={"fppath":fppath}, endpoint='filesets')
-  api.add_resource(FruitpileFileset, '/fileset/<int:id>', resource_class_kwargs={"fppath":fppath}, endpoint='fileset')
-  return api
+
+class FruitpileFileset(FruitpileResource):
+  def __init__(self, **kwargs):
+    self.fp = Fruitpile(kwargs["fppath"])
+    self.fp.open()
+    super(FruitpileFilesets,self).__init__()
+
+  def get(self):
+    pass
--- fruitpile/fruitpile_flask/v1/filesets.py
+++ fruitpile/fruitpile_flask/v1/filesets.py
@@ -15,10 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Fruitpile.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import print_function
-from flask_restful import reqparse
+from flask_restful import reqparse, fields, marshal_with
 from ..api_utils import FruitpileResource
 from ...fp_ops import Fruitpile
 import os
+
 
 class FruitpileFilesets(FruitpileResource):
   def __init__(self, **kwargs):
@@ -38,3 +39,23 @@ class FruitpileFilesets(FruitpileResource):
              "revision":fs.revision,
              "repo":fs.repo.name} for fs in fss]
     return fsss
+
+  new_fs_fields = { 'id': fields.Integer, 'url': fields.Url("fileset") }
+
+  @marshal_with(new_fs_fields)
+  def post(self):
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', help='Name of the fileset to create')
+    parser.add_argument('version', help='Version of the files contained in this fileset')
+    parser.add_argument('revision', help='Revision of the files contained in this fileset')
+    parser.add_argument('repo', help='The repo to add the filese to', default='default')
+    args = parser.parse_args()
+    fss = self.fp.add_new_fileset(uid=os.getuid(),
+                                  name=args["name"],
+                                  version=args["version"],
+                                  revision=args["revision"],
+                                  # repo ignored at this point
+                                )
+    # Location header to be figured out
+    print(fss.id)
+    return fss, 201
